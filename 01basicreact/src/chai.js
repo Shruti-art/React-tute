6eaf9b40570570d4a9d14fd973ffb996f5f76eca
shruti <pshruti176@email.com>
2024-01-20 12:51:42 +0530

    postCSS

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h1>Chai in react</h1>;
+}
+export default Chai;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  const username="chai aur code"
+  return (
+    <>
+
+     <h1>Chai aur react or {username}</h1>
+     {/* we can write only evaluated expression */}
+    <p>test para</p>
+    </>
+   
+  )
+}
+
+export default App
